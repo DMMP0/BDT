40688b8f44dd8d3e088a1952d97f79fce2517dd6
OnceByGee <maurizio.demarchi@studenti.unitn.it>
2022-06-20 18:48:01 +0200

    Source layer complete
    now the source layer sends documents as strings on redis

--- 0. Source Layer/components/document_parser.py
+++ 0. Source Layer/components/document_parser.py
@@ -9,14 +9,6 @@ from docx import Document
 # import mdtex2html
 
 
-path = 'reports'
-excel = [f for f in os.listdir(path) if f.endswith('.xlsx')]
-html = [f for f in os.listdir(path) if f.endswith('.html')]
-tex = [f for f in os.listdir(path) if f.endswith('.tex')]
-word = [f for f in os.listdir(path) if f.endswith('.docx')]
-txt = [f for f in os.listdir(path) if f.endswith('.txt')]
-
-
 def read_docx_tables(filename, tab_id=None, **kwargs):
     """
     parse table(s) from a Word Document (.docx) into Pandas DataFrame(s)
@@ -53,51 +45,58 @@ def read_docx_tables(filename, tab_id=None, **kwargs):
             raise
 
 
-print(len(excel))
+# print(len(excel))
 
 columns = ['registeration_number', 'name', 'establied_date', 'country', 'number_of_employes', 'purpose', 'phone_number',
            'email', 'bank_name', 'bank_country']
-###read excel
-for i in range(0, len(excel)):
+
+
+# read excel
+def excel_to_dict(filepath: str) -> dict:
+    """The function returns a dictionary from a filepath. The dictionary will have the filename as key and the values
+    as another dictionary"""
+    global columns
+
     # Read a text file to a dataframe using read_table function
 
-    data = pd.read_excel('reports/' + str(excel[i]))
+    data = pd.read_excel(filepath)
     data = pd.DataFrame(data)
     columns = data.columns
-    name = str(excel[i]).split('.')[0]
-    temp = data.to_json('./jsons/' + name + '.json', indent=4, orient='index')
+    name = str(filepath).split('/')[-1]
+    return {name: data.to_dict(orient='index')}
+    # temp = data.to_json('./jsons/' + name + '.json', indent=4, orient='index')
 
-for i in range(0, len(html)):
+
+def html_to_dict(filepath: str) -> dict:
+    """The function returns a dictionary from a filepath. The dictionary will have the filename as key and the values
+        as another dictionary"""
     # Read a text file to a dataframe using read_table function
-    name = str(html[i]).split('.')[0]
-    data = pd.read_html('reports/' + str(html[i]))
-    data[0].to_json('./jsons/' + name + '.json', indent=4, orient='index')
+    name = filepath.split('/')[-1]
+    data = pd.read_html(filepath)
+    return {name: data[0].to_dict(orient='index')}
+    # data[0].to_json('./jsons/' + name + '.json', indent=4, orient='index')
 
-for i in range(0, len(word)):
 
-    # Load the first table from your document. In your example file,
-    # there is only one table, so I just grab the first one.
-    document = Document('reports/' + str(word[i]))
-    table = document.tables[0]
-
-    # Data will be a list of rows represented as dictionaries
-    # containing each row's data.
-    data = []
-
-    keys = None
-    for w in word:
-        name = str(w).split('.')[0]
-        df = read_docx_tables('reports/' + str(w))
-        df = pd.DataFrame(df)
-        df = pd.DataFrame(df.values,
-                          columns=['registeration_number', 'name', 'establied_date', 'country', 'number_of_employes',
+def docx_to_dict(filepath: str) -> dict:
+    """The function returns a dictionary from a filepath. The dictionary will have the filename as key and the values
+        as another dictionary"""
+    global columns
+    name = filepath.split('/')[0]
+    df = read_docx_tables(filepath)
+    df = pd.DataFrame(df)
+    df = pd.DataFrame(df.values,
+                      columns=['registeration_number', 'name', 'establied_date', 'country', 'number_of_employes',
                                    'purpose', 'phone_number', 'email', 'bank_name', 'bank_country'])
-        df.to_json('./jsons/' + name + '.json', indent=4, orient='index')
+    return {name: df.to_dict(orient='index')}
+    # df.to_json('./jsons/' + name + '.json', indent=4, orient='index')
 
-for i in range(0, len(txt)):
+
+def txt_to_dict(filepath: str) -> dict:
+    """The function returns a dictionary from a filepath. The dictionary will have the filename as key and the values
+           as another dictionary"""
     # Read a text file to a dataframe using read_table function
-    name = str(txt[i]).split('.')[0]
-    data = pd.read_csv('reports/' + str(txt[i]), sep='\t')
+    name = str(filepath).split('/')[0]
+    data = pd.read_csv(filepath, sep='\t')
     data = pd.DataFrame(data)
-    temp2 = data.to_dict()
-    temp = data.to_json('./jsons/' + name + '.json', indent=4, orient='index')
+    return {name:data.to_dict(orient='index')}
+    # temp = data.to_json('./jsons/' + name + '.json', indent=4, orient='index')
--- 0. Source Layer/services/send_data_to_redis.py
+++ 0. Source Layer/services/send_data_to_redis.py
@@ -1,32 +1,64 @@
+import json
 import os
 import sys
 import threading
+import redis
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  # finds the parent directory
-from models.message_producer import *
-
+from components.document_parser import *
 source_layer = os.path.dirname(os.path.dirname(__file__))
 
+path = source_layer + '/components/reports/'
+excel = [f for f in os.listdir(path) if f.endswith('.xlsx')]
+html = [f for f in os.listdir(path) if f.endswith('.html')]
+# tex = [f for f in os.listdir(path) if f.endswith('.tex')]
+word = [f for f in os.listdir(path) if f.endswith('.docx')]
+txt = [f for f in os.listdir(path) if f.endswith('.txt')]
 
-broker = 'localhost:9092'  # TODO: modify
-topics = ['bank', 'broker', 'questura']
+r = redis.StrictRedis(host='localhost')  # redis object
 
-message_producer_bank = MessageProducer(broker, topics[0])
-message_producer_broker = MessageProducer(broker, topics[1])
-message_producer_questura = MessageProducer(broker, topics[2])
 
-t1 = threading.Thread(target=message_producer_bank.keep_sending_data_from_dir,
-                      args=(source_layer + "/components/reports/", True))
-t2 = threading.Thread(target=message_producer_broker.keep_sending_data_from_dir,
-                      args=(source_layer + "/components/reports/", True))
-t3 = threading.Thread(target=message_producer_questura.keep_sending_data_from_dir,
-                      args=(source_layer + "/components/reports/", True))
+def send_files_to_redis_hash(filepaths: list, format: str):
+    """This function will convert the files into dictionaries with the parser. Once done, it will send them to redis as
+    a hash that will have the filename as key and the dictionary as values.
+    @:param format: the extension of the file. Possile values: {'docx','excel','html','txt'}"""
+
+    global r  # redis instance
+    global path
+    for fp in filepaths:
+        fp = path+str(fp)
+        d = dict()
+
+        # convert into dict
+        if format == 'docx':
+            d = docx_to_dict(fp)
+        elif format == 'excel':
+            d = excel_to_dict(fp)
+        elif format == 'html':
+            d = html_to_dict(fp)
+        else:  # elif format == 'txt'
+            d = txt_to_dict(fp)
+
+        key = list(d.keys())[0]
+        # send to redis
+        r.set(name=key, value=json.dumps(d[key]))
+
+
+t1 = threading.Thread(target=send_files_to_redis_hash,
+                      args=(excel, 'excel'))
+t2 = threading.Thread(target=send_files_to_redis_hash,
+                      args=(html, 'html'))
+t3 = threading.Thread(target=send_files_to_redis_hash,
+                      args=(word, 'docx'))
+t4 = threading.Thread(target=send_files_to_redis_hash,
+                      args=(txt, 'txt'))
 
 t1.start()
 t2.start()
 t3.start()
-
+t4.start()
 # stop threads after completing the task
 t1.join()
 t2.join()
 t3.join()
+t4.join()
